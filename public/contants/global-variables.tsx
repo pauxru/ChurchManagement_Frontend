d50d4cc0fea78def4a9eab48278fa4832b38794a
Paulo <pauxru@gmail.com>
2025-03-21 10:35:43 +0300

    api

--- public/contants/global-variables.tsx
+++ public/contants/global-variables.tsx
@@ -3,6 +3,6 @@ export const CHURCH_NAME = "A.I.P.C.A";
 
 export const FULL_CHURCH_NAME = "Africa Independent Pentecostal Church of Africa";
 
-export const BASE_ENDPOINT = "http://localhost:5000";
+export const BASE_ENDPOINT = "http://localhost:5000/api";
 
 export const HOME_URL = "htts://www.pawadtech.com";
