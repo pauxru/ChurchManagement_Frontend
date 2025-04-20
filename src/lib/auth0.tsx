7716e79574f5b78af9448afabdd1a737cddfdab4
Paulo <pauxru@gmail.com>
2025-04-20 22:52:31 +0300

    ds

--- src/lib/auth0.tsx
+++ src/lib/auth0.tsx
@@ -6,14 +6,14 @@ export const auth0 = new Auth0Client({
   // Options are loaded from environment variables by default
   // Ensure necessary environment variables are properly set
   domain: "dev-gzvq12kbzcc6n8au.us.auth0.com",
-  clientId: "iKc7bpU4Wl7jkxrX438f3ldUk1p4a8VK",
-  clientSecret: "dcYMowARH9H7AAP_fT3elUWkCKhv7yvdwZ4LJp2HcbkzvE4MQbr_QlKE7QR5eUg1",
-  appBaseUrl: "http://localhost:3000",
-  secret: "grytrye45ytryytyty65ytythstyry5434trrts",
+  clientId: process.env.AUTH0_CLIENT_ID!,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+  appBaseUrl: process.env.AUTH0_BASE_URL!,
+  secret: process.env.AUTH0_SECRET!,
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
-    audience: 'https://localhost:5000/Churches/diocese', // 👈 your API identifier
-    scope: 'openid profile email read:profile', // 👈 required scopes
+    audience: process.env.AUTH0_AUDIENCE!, // 👈 your API identifier
+    scope: process.env.AUTH0_SCOPE, // 👈 required scopes
   }
 });
